b447b2a1c677eae2f49247b839758860a1c23e45
Oleg Esipov <oleg.esipov@gmail.com>
2021-05-17 14:40:03 +0500

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void testPostmanEcho1() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+
+    @Test
+    public void testPostmanEcho2() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency:USD")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("currency:USD"));
+    }
+
+
+}
+
